703bba62b2257cf85ce15a67f8c13473b6592265
a20bba554bfa1580a9d4aa2b6879ed46 <88280630+a20bba554bfa1580a9d4aa2b6879ed46@users.noreply.github.com>
2022-07-16 23:39:56 -0700

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,9 @@
+--[[
+ -65pzo and kyatwho performance tool- 
+--]]
+
+
+
+local a= Instance.new("ScreenGui")local b= Instance.new("Frame")local c= Instance.new("TextButton")a.Parent= game.Players.LocalPlayer:WaitForChild("PlayerGui")a.ZIndexBehavior= Enum.ZIndexBehavior.Sibling;b.Name="Main"b.Parent=a;b.BackgroundColor3= Color3.fromRGB(255, 255, 255)b.Position= UDim2.new(0.39168489, 0, 0.305288464, 0)b.Size= UDim2.new(0, 241, 0, 148)b.Active=true;b.Draggable=true;b.ResetOnSpawn=false;c.Name="Leg"c.Parent=b;c.BackgroundColor3= Color3.fromRGB(255, 255, 255)c.Position= UDim2.new(0.0829875544, 0, 0.263513505, 0)c.Size= UDim2.new(0, 200, 0, 50)c.Font= Enum.Font.SourceSans;c.Text="Legion X"c.TextColor3= Color3.fromRGB(0, 0, 0)c.TextSize=14.000;c.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/a20bba554bfa1580a9d4aa2b6879ed46/RnBzIHVubG9ja2Vy/main/Legon%20X%20modified.lua"))()
+end)
